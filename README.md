3547c3b3ff7278510a079c78cf26565ad0fcdbf7
Wilson Macedo <wil.macedo.sa@gmail.com>
2024-07-29 13:50:17 -0300

    feat: :rocket: project start up

---
+++ README.md
@@ -0,0 +1,1 @@
+# Web3 integration with Webflow
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Web3Modal } from "./components/web3-modal";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Web3Modal />
+    </div>
+  );
+}
+
+export default App;
