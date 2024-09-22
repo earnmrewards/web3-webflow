c738361b0a574f22b92a619e212c78fc9a1776c0
Wilson Macedo <wil.macedo.sa@gmail.com>
2024-09-22 10:51:52 -0300

    feat: :sparkles: added the input handler for the order page

--- src/components/smart-nodes/order-container.tsx
+++ src/components/smart-nodes/order-container.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useUser } from "@account-kit/react";
 import { BASE_VALUES } from "./config";
 import { useNavigate } from "../../contexts/use-navigate";
@@ -13,6 +13,8 @@ const EARN_PHONE_LABEL_ID = "web3-smart-nodes-final-phone";
 export function OrderContainer() {
   const user = useUser();
   const { back, searchParams } = useNavigate();
+  const [, setReferralCode] = useState("");
+  const [, setEmail] = useState("");
 
   const amount = Number(searchParams.get("amount"));
 
@@ -20,7 +22,7 @@ export function OrderContainer() {
     const container = document.getElementById(ORDER_CONTAINER_ID);
     if (!container) return;
 
-    container.style.display = amount <= 0 || !user ? "none" : "flex";
+    container.style.display = amount <= 0 || !user ? "none" : "block";
   }
   useEffect(changeContainerVisibility, [user, amount]);
 
@@ -41,24 +43,78 @@ export function OrderContainer() {
   function handleLabelsValue() {
     if (amount <= 0) return;
 
-    const quantityLabel = document.getElementById(QUANTITY_LABEL_ID);
-    if (!quantityLabel) return;
+    const labels = [
+      {
+        id: QUANTITY_LABEL_ID,
+        value: amount,
+      },
+      {
+        id: SMART_NODES_LABEL_ID,
+        value: parseInt(
+          String(amount >= BASE_VALUES[1] ? amount / BASE_VALUES[1] : 0)
+        ),
+      },
+      {
+        id: EARN_PHONE_LABEL_ID,
+        value: amount >= BASE_VALUES[1] ? "Yes" : "No",
+      },
+    ];
 
-    quantityLabel.innerText = String(amount);
+    for (const { id, value } of labels) {
+      const component = document.getElementById(id);
+      if (!component) continue;
 
-    const smartNodesLabel = document.getElementById(SMART_NODES_LABEL_ID);
-    if (smartNodesLabel) {
-      smartNodesLabel.innerText = parseInt(
-        String(amount >= BASE_VALUES[1] ? amount / BASE_VALUES[1] : 0)
-      ).toString();
-    }
-
-    const earnPhoneLabel = document.getElementById(EARN_PHONE_LABEL_ID);
-    if (earnPhoneLabel) {
-      earnPhoneLabel.innerText = amount >= BASE_VALUES[2] ? "Yes" : "No";
+      component.innerText = String(value);
     }
   }
   useEffect(handleLabelsValue, [amount]);
+
+  function handleInputEvent(event: Event, index: number) {
+    const target = event.target as HTMLInputElement;
+
+    index === 0 ? setReferralCode(target.value) : setEmail(target.value);
+  }
+
+  function addInputEvent() {
+    const inputs = document.querySelectorAll(`#${ORDER_CONTAINER_ID} input`);
+    if (inputs.length < 2) return;
+
+    inputs.forEach((input, index) => {
+      input.addEventListener("input", (event) =>
+        handleInputEvent(event, index)
+      );
+    });
+
+    return () => {
+      inputs.forEach((input, index) => {
+        input.removeEventListener("input", (event) =>
+          handleInputEvent(event, index)
+        );
+      });
+    };
+  }
+  useEffect(addInputEvent, []);
+
+  // useEffect(() => {
+  //   const button = document.getElementById(REVIEW_BUTTON_ID);
+  //   if (!button) return;
+
+  //   button.addEventListener("click", (event) => {
+  //     event.preventDefault();
+
+  //     console.log(referralCode);
+  //     console.log(email);
+  //   });
+
+  //   return () => {
+  //     button.removeEventListener("click", (event) => {
+  //       event.preventDefault();
+
+  //       console.log(referralCode);
+  //       console.log(email);
+  //     });
+  //   };
+  // }, [referralCode, email]);
 
   return null;
 }
