f212f692c3cbb9367bbb23f4e40e9b9d4d6d32ce
Wilson Macedo <wil.macedo.sa@gmail.com>
2024-09-30 18:47:15 -0300

    feat: :sparkles: added a staging action

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_ALCHEMY_API_KEY: string;
+  readonly VITE_API_BASE_URL: string;
+  readonly VITE_API_KEY: string;
+  readonly VITE_API_SECRET: string;
+  readonly VITE_LAMBDA_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
