e55c59c98ebe1460a58054c3328202d8d25b1cba
Wilson Macedo <wil.macedo.sa@gmail.com>
2024-10-13 02:15:25 -0300

    feat: :sparkles: added a new readme and update the html file detector func

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "node:path";
+import { readdirSync } from "node:fs";
+
+function detectHtmlFiles() {
+  const htmlFiles = readdirSync(__dirname).filter((file) =>
+    file.endsWith(".html")
+  );
+
+  const modes = htmlFiles.reduce((acc, file) => {
+    const mode = file.replace(".html", "");
+    acc[mode] = file;
+    return acc;
+  }, {} as Record<string, string>);
+
+  return modes;
+}
+
+const files = detectHtmlFiles();
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  let htmlFile = "index.html";
-
-  if (mode === "sales") {
-    htmlFile = "sales.html";
-  } else if (mode === "partner-dashboard") {
-    htmlFile = "partner-dashboard.html";
-  }
+  const defaultFile = "index.html";
+  const htmlFile = files[mode] || defaultFile;
 
   return {
     plugins: [react()],
