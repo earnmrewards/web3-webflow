e9e98b26e8fef26772de4261d5ee78c20af7dfe1
Wilson Macedo <wil.macedo.sa@gmail.com>
2024-11-11 21:55:51 -0300

    feat: :sparkles: upgraded the alchemy to fix mobile issue

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,22 @@
-import { AlchemyAccountsUIConfig, createConfig } from "@account-kit/react";
+import { createConfig } from "@account-kit/react";
 import { QueryClient } from "@tanstack/react-query";
-import { arbitrum, arbitrumSepolia } from "@account-kit/infra";
-
-const uiConfig: AlchemyAccountsUIConfig = {
-  illustrationStyle: "outline",
-  auth: {
-    sections: [[{ type: "injected" }]],
-  },
-};
+import { alchemy, arbitrum, arbitrumSepolia } from "@account-kit/infra";
 
 export const config = createConfig(
   {
-    apiKey: import.meta.env.VITE_ALCHEMY_API_KEY,
+    transport: alchemy({ apiKey: import.meta.env.VITE_ALCHEMY_API_KEY }),
     chain:
       import.meta.env.VITE_ENVIRONMENT === "production"
         ? arbitrum
         : arbitrumSepolia,
+    ssr: false,
   },
-  uiConfig
+  {
+    auth: {
+      sections: [[{ type: "external_wallets" }]],
+      addPasskeyOnSignup: false,
+    },
+  }
 );
 
 export const queryClient = new QueryClient();
