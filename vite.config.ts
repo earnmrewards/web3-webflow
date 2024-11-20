d4209afed21c3d1aff1c57667197ac2f19e5c549
Wilson Macedo <wil.macedo.sa@gmail.com>
2024-11-20 18:03:49 -0300

    chore: :zap: added the env loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "node:path";
 import { readdirSync } from "node:fs";
@@ -27,10 +27,10 @@ function buildFileName(mode: string, ext = "js") {
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const defaultFile = "index.html";
-  const files = detectHtmlFiles();
-  const htmlFile = files[mode] || defaultFile;
-  const baseURL = process.env.VITE_BASE_URL || "/";
+  const env = loadEnv(mode, process.cwd());
+
+  const entryFileName = detectHtmlFiles()[mode] || "index.html";
+  const baseURL = env.VITE_BASE_URL || "/";
 
   return {
     base: baseURL,
@@ -50,7 +50,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
-      open: `./${htmlFile}`,
+      open: `./${entryFileName}`,
     },
   };
 });
