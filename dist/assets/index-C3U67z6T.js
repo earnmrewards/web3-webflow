0fe75a149fda1620cee03de2b9ebfc7977ef87d4
Wilson Macedo <wil.macedo.sa@gmail.com>
2025-01-14 11:00:24 -0300

    chore: :zap: updated version for staging

--- dist/assets/index-C3U67z6T.js
+++ dist/assets/index-C3U67z6T.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-B4PHJzM8.js";import"../plugin-staging-1.2.5.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-C32gSlww.js";import"../plugin-staging-1.2.6.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
