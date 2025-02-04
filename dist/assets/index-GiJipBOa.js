d5f4865ff204f48bca6f38853920c6ab23783ed7
Wilson Macedo <wil.macedo.sa@gmail.com>
2025-02-04 16:01:35 -0300

    chore: :zap: updated package version

--- dist/assets/index-GiJipBOa.js
+++ dist/assets/index-GiJipBOa.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-CX7RvArV.js";import"../plugin-staging-1.3.40.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-DeH5Ztgu.js";import"../plugin-staging-1.3.41.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
