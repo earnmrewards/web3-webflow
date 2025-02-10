6612c03f2318fa8b4a5484584f886f401eced300
Wilson Macedo <wil.macedo.sa@gmail.com>
2025-02-10 10:27:28 -0300

    chore: :zap: updated version

--- dist/assets/index-Bq8wUDFX.js
+++ dist/assets/index-Bq8wUDFX.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-DTMrMrfl.js";import"../plugin-staging-1.3.48.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-Z37REJQr.js";import"../plugin-staging-1.3.49.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
