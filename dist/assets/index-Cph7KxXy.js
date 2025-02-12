649065a6ee1c7eb4273c05426fe12391974a7d62
Wilson Macedo <wil.macedo.sa@gmail.com>
2025-02-12 11:19:22 -0300

    chore: :zap: updated builded version

--- dist/assets/index-Cph7KxXy.js
+++ dist/assets/index-Cph7KxXy.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-C2zxN822.js";import"../plugin-staging-1.3.60.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-D-Q_MQwu.js";import"../plugin-staging-1.3.61.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
