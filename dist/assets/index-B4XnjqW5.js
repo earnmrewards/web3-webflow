3751c4c535ddbfee163f0bfe8c16761bd63cbb22
Wilson Macedo <wil.macedo.sa@gmail.com>
2025-02-25 12:17:14 -0300

    chore: :zap: updated package version

--- dist/assets/index-B4XnjqW5.js
+++ dist/assets/index-B4XnjqW5.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-CBuxc98J.js";import"../plugin-1.4.18.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-CX6nI-aU.js";import"../plugin-1.4.19.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
