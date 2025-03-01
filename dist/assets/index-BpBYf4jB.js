6a65fe3cfbcbcf318cb708b14a039802da410e43
Wilson Macedo <wil.macedo.sa@gmail.com>
2025-03-01 11:56:33 -0300

    fix: :bug: updated timestamp to reward pool

--- dist/assets/index-BpBYf4jB.js
+++ dist/assets/index-BpBYf4jB.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-8JswV9kK.js";import"../plugin-staging-1.4.26.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-CrJh6D-I.js";import"../plugin-staging-1.4.27.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
