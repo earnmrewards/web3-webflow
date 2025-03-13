625ffaf64bb5669dfe996ae391e33a9c9f544178
Wilson Macedo <wil.macedo.sa@gmail.com>
2025-03-13 12:49:50 -0300

    fix: :bug: updated table history

--- src/features/staking/staking-component/lists/history-list.tsx
+++ src/features/staking/staking-component/lists/history-list.tsx
@@ -47,7 +47,7 @@ export function HistoryList({ page }: HistoryListProps) {
 
   return (
     <>
-      <div className="mt-3 pr-2 grid py-0 grid-cols-1 gap-4 md:max-h-[490px] overflow-y-auto custom-scrollbar">
+      <ul className="mt-3 pr-2 grid py-0 grid-cols-1 gap-4 md:max-h-[490px] overflow-y-auto custom-scrollbar">
         {data &&
           data.history.map(
             (
@@ -60,76 +60,89 @@ export function HistoryList({ page }: HistoryListProps) {
               },
               index
             ) => (
-              <div
+              <li
                 key={index}
-                className="min-w-fit w-full flex flex-row justify-between gap-4 border border-[#C5C5C5] rounded-2xl p-5 overflow-x-auto custom-thin-scrollbar"
+                className="min-w-fit w-full flex flex-row items-center justify-center border border-[#C5C5C5] rounded-2xl p-5"
               >
-                <div className="flex flex-col">
-                  <span className="text-block-24 font-galano whitespace-nowrap">
-                    Date
-                  </span>
-                  <span className="heading-text-table text-white font-bold">
-                    {new Date(actionTimestamp * 1000).toLocaleDateString()}
-                  </span>
-                </div>
-                <div className="flex flex-col whitespace-nowrap">
-                  <span className="text-block-24 font-galano">Hash</span>
-                  <a
-                    href={`${explorerUrl}/${hash}`}
-                    target="_blank"
-                    className="heading-text-table text-white font-bold underline"
-                  >
-                    {shortenAddress(hash)}
-                  </a>
+                <div className="flex flex-1 justify-center">
+                  <div className="flex flex-col w-24">
+                    <span className="text-block-24 font-galano whitespace-nowrap">
+                      Date
+                    </span>
+                    <span className="heading-text-table text-white font-bold">
+                      {new Date(actionTimestamp * 1000).toLocaleDateString()}
+                    </span>
+                  </div>
                 </div>
 
-                <div className="flex flex-col">
-                  <span className="text-block-24 font-galano whitespace-nowrap">
-                    Node IDs
-                  </span>
-                  <a
-                    className="heading-text-table text-white font-bold underline cursor-pointer whitespace-nowrap"
-                    {...getNodeProperties(smartNodeIds)}
-                    onClick={() => handleSingleNodeClick(smartNodeIds, hash)}
-                  >
-                    {smartNodeIds.length === 1 ? smartNodeIds[0] : "View List"}
-                  </a>
+                <div className="flex flex-1 justify-center">
+                  <div className="flex flex-col w-32">
+                    <span className="text-block-24 font-galano">Hash</span>
+                    <a
+                      href={`${explorerUrl}/${hash}`}
+                      target="_blank"
+                      className="heading-text-table text-white font-bold underline"
+                    >
+                      {shortenAddress(hash)}
+                    </a>
+                  </div>
                 </div>
 
-                <div className="flex flex-col">
-                  <span className="text-block-24 font-galano whitespace-nowrap">
-                    Amount of Nodes
-                  </span>
-                  <span className="heading-text-table text-white font-bold">
-                    {smartNodeIds.length.toLocaleString()}
-                  </span>
+                <div className="flex flex-1 justify-center">
+                  <div className="flex flex-col w-24">
+                    <span className="text-block-24 font-galano whitespace-nowrap">
+                      Node IDs
+                    </span>
+                    <a
+                      className="heading-text-table text-white font-bold underline cursor-pointer whitespace-nowrap"
+                      {...getNodeProperties(smartNodeIds)}
+                      onClick={() => handleSingleNodeClick(smartNodeIds, hash)}
+                    >
+                      {smartNodeIds.length === 1
+                        ? smartNodeIds[0]
+                        : "View List"}
+                    </a>
+                  </div>
                 </div>
 
-                <div className="flex flex-col">
-                  <span className="text-block-24 font-galano whitespace-nowrap">
-                    Action
-                  </span>
-                  <div className="flex items-center gap-2 px-3 py-0.5 bg-[#00420F] rounded-full">
-                    <div className="flex items-center gap-1 text-[#00D632] font-bold">
-                      <span className="heading-text-table capitalize text-sm">
-                        {typeName[actionType]}
-                      </span>
-                      {actionType === "claim" && (
-                        <>
-                          <span className="text-xs">+</span>
-                          <InvertedCoin className="w-4 h-4" />
-                          {claimedAmountEther.toLocaleString(undefined, {
-                            maximumFractionDigits: 2,
-                          })}
-                        </>
-                      )}
+                <div className="flex flex-1 justify-center">
+                  <div className="flex flex-col w-32">
+                    <span className="text-block-24 font-galano whitespace-nowrap">
+                      Amount of Nodes
+                    </span>
+                    <span className="heading-text-table text-white font-bold">
+                      {smartNodeIds.length.toLocaleString()}
+                    </span>
+                  </div>
+                </div>
+
+                <div className="flex flex-1 justify-center">
+                  <div className="flex flex-col w-fit">
+                    <span className="text-block-24 font-galano whitespace-nowrap">
+                      Action
+                    </span>
+                    <div className="flex items-center gap-2 px-3 py-0.5 bg-[#00420F] rounded-full">
+                      <div className="flex items-center gap-1 text-[#00D632] font-bold">
+                        <span className="heading-text-table capitalize text-sm">
+                          {typeName[actionType]}
+                        </span>
+                        {actionType === "claim" && (
+                          <>
+                            <span className="text-xs">+</span>
+                            <InvertedCoin className="w-4 h-4" />
+                            {claimedAmountEther.toLocaleString(undefined, {
+                              maximumFractionDigits: 2,
+                            })}
+                          </>
+                        )}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              </li>
             )
           )}
-      </div>
+      </ul>
     </>
   );
 }
